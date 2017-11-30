9bde9a93e743ea361fdcde78d8c7c34797804ddb
nic86 <n.eva@simplesolutions.it>
2017-11-30 19:16:54 +0100

    aggiornato travis CI

---
+++ languages/it_it/Documenti.php
@@ -0,0 +1,11 @@
+<?php
+/* {[The file is published on the basis of YetiForce Public License that can be found in the following directory: licenses/License.html]} */
+$languageStrings = [
+	'Documenti' => 'Documenti',
+	'SINGLE_Documenti' => 'Documenti',
+	//BLOCKS
+	'LBL_CUSTOM_INFORMATION' => 'Custom Information',
+	'LBL_BASIC_INFORMATION' => 'Basic Information',
+	//FIELDS
+	'nome' => 'Nome',
+];
