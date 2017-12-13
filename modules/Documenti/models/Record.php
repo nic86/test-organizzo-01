451fbe753fbd9965f2eb9570fea710ae80c98c71
nic86 <n.eva@simplesolutions.it>
2017-12-13 10:20:20 +0100

    fix codice non utilizzato modulo documenti

--- modules/Documenti/models/Record.php
+++ modules/Documenti/models/Record.php
@@ -75,11 +75,6 @@ class Documenti_Record_Model extends Vtiger_Record_Model
 		return false;
 	}
 
-	public function incrementVersion()
-	{
-		$id = $this->getId();
-	}
-
 	public function downloadFile()
 	{
 		if (!$this->checkFileIntegrity()) {
@@ -235,8 +230,6 @@ class Documenti_Record_Model extends Vtiger_Record_Model
 					return false;
 				}
 			} else {
-				$arrmeta = stream_get_meta_data($uploadFilePath . DIRECTORY_SEPARATOR. $fileName);
-				$arrmeta2 = get_meta_tags($uploadFilePath . DIRECTORY_SEPARATOR. $fileName);
 				if(!$this->validateUploadedFileName($fileName)) {
 					unset($this->changes['docfilename']);
 					return false;
