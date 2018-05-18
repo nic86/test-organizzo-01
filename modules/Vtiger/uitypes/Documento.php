0902011d90e7090daa56e1e13a87c7d6c6ccd7a2
nic86 <n.eva@simplesolutions.it>
2018-05-18 18:01:33 +0200

    aggiornato documenti

--- modules/Vtiger/uitypes/Documento.php
+++ modules/Vtiger/uitypes/Documento.php
@@ -33,9 +33,47 @@ class Vtiger_Documento_UIType extends Vtiger_Base_UIType
 	{
 		if ($recordInstance && !empty($value)) {
 			$moduleName = $recordInstance->getModuleName();
-			$filename = basename($value);
-			$fieldname = $this->get('field')->name;
+			$filepath   = $value;
+			$filename   = basename($value);
+			$fieldname  = $this->get('field')->name;
+			$filetype   = \App\Fields\File::getMimeContentType($filepath);
+
+			if(file_exists($filepath)) {
+				$fileEncoded = base64_encode(file_get_contents($filepath));
+			}
+
 			$value ="<a class='btn btn-default btn-sm popoverTooltip' href='file.php?module=" . $moduleName .'&action=DownloadFile&record=' . $record . '&fieldname=' . $fieldname . "' data-content='Apri Documento'><span class='glyphicon glyphicon-open-file' aria-hidden='true'></span></a>&nbsp;&nbsp;";
+
+			$ext = explode('.', $filename);
+			$ext = strtolower(array_pop($ext));
+			if (in_array($ext, self::$allowedPreviewFormats)) {
+				$value .="<a class='btn btn-default btn-sm popoverTooltip' target='_blank' href='file.php?module=" . $moduleName .'&action=DownloadFile&record=' . $record . '&fieldname=' . $fieldname ."&show=true' data-content='Anteprima Documento'><span class='glyphicon glyphicon-search' aria-hidden='true'></span></a>&nbsp;&nbsp;";
+			}
+			$value .= $filename. "<br/><br/>";
+			if (!empty($filepath) && (strpos($filetype, 'image') !== FALSE)){
+			   $value .= "<img src='data:{$filetype};base64,{$fileEncoded}' width='100%'>";
+			}
+		}
+		return $value;
+	}
+
+	public function getListViewDisplayValue($value, $record = false, $recordInstance = false, $rawText = false)
+    {
+        if ($recordInstance && !empty($value)) {
+			$moduleName = $recordInstance->getModuleName();
+			$filepath   = $value;
+			$filename   = basename($value);
+			$fieldname  = $this->get('field')->name;
+			$filetype   = \App\Fields\File::getMimeContentType($filepath);
+
+			$value= '';
+
+			if(file_exists($filepath) && (strpos($filetype, 'image') !== FALSE)) {
+				$fileEncoded = base64_encode(file_get_contents($filepath));
+			   	$value .= "<img src='data:{$filetype};base64,{$fileEncoded}' height='100px'><br/>";
+			}
+
+			$value .="<a class='btn btn-default btn-sm popoverTooltip' href='file.php?module=" . $moduleName .'&action=DownloadFile&record=' . $record . '&fieldname=' . $fieldname . "' data-content='Apri Documento'><span class='glyphicon glyphicon-open-file' aria-hidden='true'></span></a>&nbsp;&nbsp;";
 
 			$ext = explode('.', $filename);
 			$ext = strtolower(array_pop($ext));
@@ -45,11 +83,7 @@ class Vtiger_Documento_UIType extends Vtiger_Base_UIType
 			$value .= $filename;
 		}
 		return $value;
-	}
 
-	public function getListViewDisplayValue($value, $record = false, $recordInstance = false, $rawText = false)
-    {
-        return $this->getDisplayValue($value, $record, $recordInstance, $rawText);
 	}
 
 }
